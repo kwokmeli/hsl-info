23d8753a331115c9ead6bf5f09f46e7e91ae6a73
mlkwok <mkwok@uw.edu>
2017-09-29 10:53:31 -0700

    finished basic information retrieval for pubmed articles

--- src/getPubMed.js
+++ src/getPubMed.js
@@ -1,14 +1,16 @@
 "use strict";
 const https = require("https");
 const xml2js = require("xml2js");
-const nodemailer = require("nodemailer");
+//const nodemailer = require("nodemailer");
+
+const NA = "N/A";
 
 // Subject to search for
-var subject = "malaria";
+var subject = "epidemiology";
 var subjectCopy = subject;
 
 // Maximum number of results to list
-var retMax = 20;
+var retMax = 15;
 var articles = [];
 
 function encode() {
@@ -27,7 +29,6 @@ var encodeFirst = new Promise(function (fulfill, reject) {
 	fulfill();
 });
 
-// TODO: Add error-checking
 encodeFirst.then(function () {
   var searchURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmode=json&retmax=" + retMax + "&term=" + subject;
 
@@ -45,7 +46,11 @@ encodeFirst.then(function () {
         articles.push(body.esearchresult.idlist[i]);
       }
 
-			printRandomArticle();
+			// Select random article
+			var articleNumber = articles[Math.floor(Math.random() * articles.length)];
+
+			// Select
+			printArticle(articleNumber);
     });
   });
 });
@@ -54,12 +59,9 @@ encodeFirst.catch(function () {
   console.log("Invalid encoding.");
 });
 
-function printRandomArticle () {
-	var article = articles[Math.floor(Math.random() * articles.length)];
-console.log("random article: " + article);
-
-	var abstractURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract&id=" + article;
-	//var abstractURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract&id=28953262";
+function printArticle(articleNumber) {
+	// Retrieve article using Pub Med ID
+	var abstractURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&retmode=xml&rettype=abstract&id=" + articleNumber;
 
 	https.get(abstractURL, function (res) {
 		res.setEncoding("utf8");
@@ -71,27 +73,74 @@ console.log("random article: " + article);
 		res.on("end", function () {
 			var parser = new xml2js.Parser();
 			parser.parseString(body, function(err, result) {
-//console.log(JSON.stringify(result));
-			extractDetails(result);
+			extractDetails(result, article);
 			});
 		});
 	});
 }
 
-function extractDetails(result) {
-	var volume = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["JournalIssue"][0]["Volume"];
-	var issue = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["JournalIssue"][0]["Issue"];
-	var journalTitle = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["Title"];
-	var articleTitle = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleTitle"];
-	var abstract = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Abstract"][0]["AbstractText"];
-
-	// All dates are in the form YYYY, MM, DD
-	var datePublished = [result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleDate"][0]["Year"],
-											 result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleDate"][0]["Month"],
-										 	 result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleDate"][0]["Day"]];
+function extractDetails(result, article) {
+	var journalTitle, articleTitle;
+	var volume, issue;
+	var issn, issnType;
+	var datePublished = [];
 	var dateReceived = [];
 	var dateRevised = [];
 	var dateAccepted = [];
+	var abstract = "";
+	var authorsFirst = [];
+	var authorsLast = [];
+	var authorsAffiliation = [];
+
+	// Retrieve journal title if it exists
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["Title"]) {
+		journalTitle = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["Title"];
+	} else {
+		journalTitle = NA;
+	}
+
+	// Retrieve article title if it exists
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleTitle"]) {
+		articleTitle = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleTitle"];
+	} else {
+		articleTitle = NA;
+	}
+
+	// Retrieve volume if it exists
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["JournalIssue"][0]["Volume"]) {
+		volume = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["JournalIssue"][0]["Volume"];
+	} else {
+		volume = NA;
+	}
+
+	// Retrieve issue if it exists
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["JournalIssue"][0]["Issue"]) {
+		issue = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["JournalIssue"][0]["Issue"];
+	} else {
+		issue = NA;
+	}
+
+	// Retrieve ISSN number if it exists
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["ISSN"][0]["_"]){
+		issn = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["ISSN"][0]["_"];
+	} else {
+		issn = NA;
+	}
+
+	// Retrieve ISSN type if it exists
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["ISSN"][0]["$"]["IssnType"]) {
+		issnType = result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Journal"][0]["ISSN"][0]["$"]["IssnType"];
+	} else {
+		issnType = NA;
+	}
+
+	// All dates are in the form YYYY, MM, DD
+	// Retrieve publication dates if they exist
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleDate"]) {
+		datePublished.push(result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleDate"][0]["Year"],
+											 result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleDate"][0]["Month"],
+											 result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["ArticleDate"][0]["Day"]);
+	}
 
 	// Retrieve dates for when article was received, revised, and accepted, if available
 	for (var i = 0; i < result["PubmedArticleSet"]["PubmedArticle"][0]["PubmedData"][0]["History"][0]["PubMedPubDate"].length; i++) {
@@ -111,71 +160,50 @@ function extractDetails(result) {
 		}
 	}
 
-	var authorsFirst = [];
-	var authorsLast = [];
-	var authorsAffiliation = [];
-
-	// Extract all authors
-	for (var i = 0; i < result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"].length; i++) {
-		var aff = [];
-		authorsFirst.push(result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["ForeName"]);
-		authorsLast.push(result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["LastName"]);
-
-		// Affiliations include extraneous information
-		// for (var j = 0; j < result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["AffiliationInfo"].length; j++) {
-		// 	aff.push(result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["AffiliationInfo"][j]["Affiliation"]);
-		// }
-		// authorsAffiliation.push(aff);
-	}
-
-	if (volume) {
-		console.log("Volume: " + volume);
-	}
-
-	if (issue) {
-		console.log("Issue: " + issue);
-	}
-
-	if (journalTitle) {
-		console.log("Journal Title: " + journalTitle);
-	}
-
-	if (articleTitle) {
-		console.log("Article Title: " + articleTitle);
-	}
-
-	if (authorsFirst || authorsLast) {
-		var authors = "";
-		for (var i = 0; i < authorsFirst.length; i++) {
-			if (i == authorsFirst.length - 2) {
-				authors += authorsFirst[i] + " " + authorsLast[i] + ", and ";
-			} else if (i == authorsFirst.length - 1) {
-				authors += authorsFirst[i] + " " + authorsLast[i] + ". ";
+	// Retrieve abstract if it exists
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Abstract"]) {
+		for (var i = 0; i < result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Abstract"][0]["AbstractText"].length; i++) {
+			if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Abstract"][0]["AbstractText"][i]["_"]) {
+				abstract += result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Abstract"][0]["AbstractText"][i]["_"] + " ";
 			} else {
-				authors += authorsFirst[i] + " " + authorsLast[i] + ", ";
+				abstract += result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["Abstract"][0]["AbstractText"][i];
 			}
 		}
-		console.log("Authors: " + authors);
+	} else {
+		abstract = NA;
 	}
 
-	if (abstract) {
-		console.log("Abstract: " + abstract);
+	// Retrieve all authors if they exist
+	if (result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"]) {
+		for (var i = 0; i < result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"].length; i++) {
+			var aff = [];
+			authorsFirst.push(result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["ForeName"]);
+			authorsLast.push(result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["LastName"]);
+
+			// Affiliations include extraneous information
+			// for (var j = 0; j < result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["AffiliationInfo"].length; j++) {
+			// 	aff.push(result["PubmedArticleSet"]["PubmedArticle"][0]["MedlineCitation"][0]["Article"][0]["AuthorList"][0]["Author"][i]["AffiliationInfo"][j]["Affiliation"]);
+			// }
+			// authorsAffiliation.push(aff);
+		}
 	}
 
-	if (datePublished) {
-		console.log("Date Published: " + datePublished);
-	}
+	console.log("Article title: " + articleTitle + "\nJournal title: " + journalTitle + "\nVolume: " + volume + "\nIssue: " + issue +
+							"\nISSN: " + issn + "\nISSN type: " + issnType + "\nPub Med ID: " + article + "\nDate published: " + datePublished + "\nDate received: " + dateReceived +
+							"\nDate revised: " + dateRevised + "\nDate accepted: " + dateAccepted);
 
-	if (dateReceived) {
-		console.log("Date Received: " + dateReceived);
+	var authors = "";
+	for (var i = 0; i < authorsFirst.length; i++) {
+		if (i == authorsFirst.length - 2) {
+			authors += authorsFirst[i] + " " + authorsLast[i] + ", and ";
+		} else if (i == authorsFirst.length - 1) {
+			authors += authorsFirst[i] + " " + authorsLast[i] + ". ";
+		} else {
+			authors += authorsFirst[i] + " " + authorsLast[i] + ", ";
+		}
 	}
+	console.log("Authors: " + authors);
 
-	if (dateRevised) {
-		console.log("Date Revised: " + dateRevised);
-	}
-
-	if (dateAccepted) {
-		console.log("Date Accepted: " + dateAccepted);
-	}
+	console.log("Abstract: " + abstract);
 
 }
